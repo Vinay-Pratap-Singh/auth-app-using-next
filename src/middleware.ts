9d34893aa2aa9e97bb6fd3bf23fdb8c50c1d6210
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-08-13 22:29:59 +0530

    feat: protected the routes

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value || "";
+  const currentPath = request.nextUrl.pathname;
+  const isPublicPath =
+    currentPath === "/" ||
+    currentPath === "/login" ||
+    currentPath === "/signup";
+  if (token && isPublicPath) {
+    return NextResponse.redirect(new URL("/profile", request.url));
+  }
+  if (!token && !isPublicPath) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+// matching the routes
+export const config = {
+  matcher: ["/", "/login", "/signup", "/profile"],
+};
