ba04fb0fe43520bd0b23eeda48534e0cd7e6759c
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-08-14 12:55:05 +0530

    feat: added route for user account verification

---
+++ src/app/api/user/verify/route.ts
@@ -0,0 +1,45 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+// connecting with the database
+connectDB();
+
+// function to verify the user account
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+
+    //   checking the user based on the token
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    //   user not found
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+
+    //   updating the user fields if it exist
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+
+    return NextResponse.json({
+      success: true,
+      message: "User verification successfull",
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      success: false,
+      message: "Failed to verify the user",
+      error: error?.message,
+    });
+  }
+}
