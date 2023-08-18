5ac082fa3a4d09b66fac59f7d99930c9437802fe
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-08-18 15:02:20 +0530

    fix: delete cookie on logout

--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -9,10 +9,7 @@ export async function GET() {
       },
       { status: 200 }
     );
-    response.cookies.set("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    });
+    response.cookies.delete("token");
     return response;
   } catch (error) {
     return NextResponse.json(
